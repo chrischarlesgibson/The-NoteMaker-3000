a62f5b7be0e882b56cee95907f294d48f39b5875
Chris Gibson <chrischarlesgibson@gmail.com>
2022-10-26 19:28:08 -0500

    push fixed command line error

--- routes/server.js
+++ routes/server.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const express = require("express");
 const miniApp = require("express").Router();
 const path = require("path");
-
+const app = express();
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -13,7 +13,7 @@ const htmlRouter = require("../routes/html");
 const apiRouter = require("../routes/api");
 
 //making new instance of express
-const app = express();
+
 const PORT = 3000;
 
 //requiring mini app to use the modular routers
