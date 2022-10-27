d535e6d5bd800ebd39140b571246aeda45a89477
Chris Gibson <chrischarlesgibson@gmail.com>
2022-10-26 19:36:43 -0500

    push for bcs

--- routes/server.js
+++ routes/server.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const express = require("express");
 const miniApp = require("express").Router();
 const path = require("path");
-const app = express();
+
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
